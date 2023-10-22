60b5f671ea9cdb11f79094ebc7485afb377a9068
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2023-10-23 00:28:19 +0200

    Fixes for step-2

--- step-2/packages/web/index.ts
+++ step-2/packages/web/index.ts
@@ -4,7 +4,7 @@ const a = Math.round(Math.random() * 100);
 const b = Math.round(Math.random() * 100);
 
 const c = calculate(a, b) * 2;
-console.log(`The double of ${a} and ${b} is ${c}`);
+console.log(`The double of the sum of ${a} and ${b} is ${c}`);
 
 window.addEventListener('load', () => {
     var text = `The sum of ${a} and ${b} is ${calculate(a, b)}\n`;
